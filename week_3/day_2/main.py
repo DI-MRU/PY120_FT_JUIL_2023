5d94e9774280467c635c674e5a933ff27cec2121
Nirmal <Nirmal@admin.com>
2023-07-25 15:36:08 +0400

    new

---
+++ week_3/day_2/main.py
@@ -0,0 +1,12 @@
+from common import test #import only test function
+from common import * #import all functions from library
+from common import test as ts #import test function and rename it to ts
+
+
+test()
+ts()
+
+
+# import common
+
+# common.test() #call test function from common library
